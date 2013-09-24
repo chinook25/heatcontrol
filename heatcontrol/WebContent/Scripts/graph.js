c76a2babd6f9542a79d801bf2d1e6eb1e4ac4b57
Wolter <Wolter@Wolter_Laptop>
2013-09-24 16:53:48 +0200

    calling external graph script

--- heatcontrol/WebContent/Scripts/graph.js
+++ heatcontrol/WebContent/Scripts/graph.js
@@ -1,12 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Highcharts Example</title>
-
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-		<script type="text/javascript">
-$(function () {
+function graph() {
+	alert("Hello world");
     $(document).ready(function() {
         Highcharts.setOptions({
             global: {
@@ -83,14 +76,4 @@ $(function () {
         });
     });
     
-});
-		</script>
-	</head>
-	<body>
-<script src="../Highchart/js/highcharts.js"></script>
-<script src="../Highchart/js/modules/exporting.js"></script>
-
-<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-
-	</body>
-</html>
+};
