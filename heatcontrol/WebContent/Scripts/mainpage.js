a5e318d8a458447fc47534fee5f919be2fd8acda
Wolter <Wolter@Wolter_Laptop>
2013-09-27 11:39:53 +0200

    Update to Webcontent

---
+++ heatcontrol/WebContent/Scripts/mainpage.js
@@ -0,0 +1,13 @@
+// mainpage.js is used to load from the database all room instances 
+// and display the internal temperature in a graph.
+$(document).ready(function() {
+	
+	
+});
+
+<h2>Heading</h2>
+<p>
+	<div id='graph1'></div>
+	<script>graph('#graph1')</script>
+<p><a class="btn btn-default" href="roomInformation.html?id=room1">View details &raquo;</a></p>
+</div><!--/span-->
---
+++ heatcontrol/WebContent/Scripts/roomInfo.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	var url = document.createElement('a'); 
+    url.href = window.location.href;
+    roomID = (url.search).substr(4); 
+   
+    // draw graph of room temperature
+    
+    
+    // show weather forecast
+    
+    
+    
+    // show calendar
+    		 
+});
