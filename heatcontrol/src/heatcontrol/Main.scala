9c53a03936db4de0b560c5f4a9ce94b683ad6a44
Joris <chinook25@gmail.com>
2013-09-27 10:46:41 +0200

    push to get to 2.9.x

--- heatcontrol/src/heatcontrol/Main.scala
+++ heatcontrol/src/heatcontrol/Main.scala
@@ -6,7 +6,7 @@ object HelloWorld {
   def main(args: Array[String]) {
     println("Hallo, wereld!")
     val mongoClient = MongoClient("localhost", 27017)
-    val db = mongoClient("test1")
+    val db = mongoClient("test")
     db.collectionNames
   }
 }
